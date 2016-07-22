53dc87ff0a251fac048d6695730b88662b9f830b
IGI-111 <igi.222@gmail.com>
2016-07-22 17:35:01 +0200

    using new addon api's child_process

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var data = require("sdk/self").data;
 var tabs = require("sdk/tabs");
 var simple_prefs = require("sdk/simple-prefs");
-const {Cc,Ci} = require("chrome");
+var child_process = require("sdk/system/child_process");
+var system = require("sdk/system");
 var contextMenu = require("sdk/context-menu");
 
 function play_video(url) {
-    var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
-    file.initWithPath(simple_prefs.prefs.player);
+    let args = [];
+    if(simple_prefs.prefs.params !== ''){
+        args = simple_prefs.prefs.params.split(" ");
+    }
 
-    var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
-    process.init(file);
+    args.push(url);
 
-    var args = simple_prefs.prefs.params.split(" ")
-    args.push(url)
+    let ls = child_process.spawn(simple_prefs.prefs.player, args, {
+        shell: true,
+        env: { DISPLAY: system.env.DISPLAY, XAUTHORITY: system.env.XAUTHORITY }
+    });
 
-    process.runAsync(args, args.length);
+    ls.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+
+    ls.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
+    ls.on('close', function (code) {
+        console.log('child process exited with code ' + code);
+    });
 }
 
 function play_current_url() {
